6e4c1228438a1f8f738eff00926130e63e314023
temirlan <temirbltv.tem@gmail.com>
2024-01-19 23:19:55 +0600

    New branch

--- week1/syntax.py
+++ week1/syntax.py
@@ -1,8 +1,11 @@
 x = 5
 y = 2
 z = "Hello, World!"
-if 5 > 2:
+if x > y:
  print("Five is greater than two!") 
-if 5 > 2:
+if x > y:
         print("Five is greater than two!") 
 print(z)
+
+#variables assigned to ints x = 5, y = 2, and string z = "Hello, World!"
+#two if operators compare x and z, then output string and variable z
